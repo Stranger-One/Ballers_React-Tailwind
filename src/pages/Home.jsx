e2e998a5eecca9b4f1f8c6b9af65dd61b9a0a545
Dolamani Rohidas <dolamanirohidas696@gmail.com>
2025-03-08 02:05:21 +0530

    add heading for all products section in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -124,6 +124,8 @@ const Home = () => {
         </div>
       )}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <h1 className="text-3xl font-bold col-span-full">All Products</h1>
+
         {filteredItems.length ? filteredItems.map((item) => (
           <div
             key={item.id}
